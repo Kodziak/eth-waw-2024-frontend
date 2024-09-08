3e27a6c43eadd10ccdcde8ab09fbd8c73aa5998e
Kodziak <kodziak1416@gmail.com>
2024-09-08 04:42:46 +0200

    clean code

--- src/utils/get-progress.ts
+++
@@ -1,9 +0,0 @@
-const AZERO_TO_LP = 5_000;
-
-export const getProgress = (freeBalanceFormatted: string | undefined) => {
-  return (
-    (parseFloat(freeBalanceFormatted?.split(" ")[0].replaceAll(",", "")!) /
-      AZERO_TO_LP) *
-    100
-  ).toLocaleString();
-};
